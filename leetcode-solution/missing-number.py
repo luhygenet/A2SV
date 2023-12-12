bcfda99fe3e6edae58006c1e658a430d9a42e5d4
luhygenet <149268849+luhygenet@users.noreply.github.com>
2023-12-13 01:28:27 +0300

    Added solution for Missing Number

---
+++ leetcode-solution/missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+      nums.sort()
+      print(nums)
+      for i in range (len(nums)+1):
+        if i == len(nums):
+            return i
+        if nums[i] != i:
+            return i
+        
