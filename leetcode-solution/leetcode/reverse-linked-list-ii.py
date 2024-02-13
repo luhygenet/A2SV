cc26e3747d600e99295a93aa76164c5e3e097ed8
luhygenet <149268849+luhygenet@users.noreply.github.com>
2024-02-13 23:19:08 +0300

    Add solution for Reverse Linked List II

---
+++ leetcode-solution/leetcode/reverse-linked-list-ii.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        dummy = ListNode(0, next = head)
+        prev = dummy
+        i = 1
+        while i < left:
+            prev = prev.next
+            i += 1
+        #prev is now the element right before the reversed portion
+        print(prev)
+        curr = prev.next
+        nx = curr.next
+
+        while i < right:
+            tmp = nx.next
+            nx.next = curr
+            curr = nx
+            nx = tmp
+            i += 1
+        # nx will be the element right after the reversed portion 
+        print(prev)
+        prev.next.next = nx
+        prev.next = curr
+
+        return dummy.next
